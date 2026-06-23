55da56e467d0da7039340f3399b83cdc59bd0017
MB-Development <mathieu_broe@yahoo.com>
2026-06-23 10:38:32 +0200

    Added service registrations

--- InThePocket.Assignment.GardenManager.Ports/RegisterGardenManager.cs
+++ InThePocket.Assignment.GardenManager.Ports/RegisterGardenManager.cs
@@ -1,10 +1,16 @@
+using InThePocket.Assignment.GardenManager.Application;
+using InThePocket.Assignment.GardenManager.Application.Mappers.Garden;
+using InThePocket.Assignment.GardenManager.Application.Services.Garden;
+using InThePocket.Assignment.GardenManager.Ports.Database;
+
 namespace InThePocket.Assignment.GardenManager.Ports;
 
 public static class RegisterGardenManager
 {
     public static void RegisterDependencies(this IServiceCollection services)
     {
-        //services.AddScoped<IGardenService, GardenService>();
-        //services.AddScoped<IGardenMapper, GardenMapper>();
+        services.AddScoped<IGardenService, GardenService>();
+        services.AddScoped<IGardenMapper, GardenMapper>();
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
     }
 }
